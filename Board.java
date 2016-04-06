23d0e823b809665f93c4a71f6c26cb03024ed40e
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2016-04-06 05:12:05 -0500

    Board can be initialized and can be displayed

---
+++ Board.java
@@ -0,0 +1,41 @@
+public class Board {
+    private String[][] board;
+
+    public Board() {};
+    public Board(int size) {
+        String[][] board = createBoard(size);
+        this.board = initializeBoard(board);
+    }
+
+    public String[][] createBoard(int size) {
+        String[][] board = new String[size][size];
+        return board;
+    }
+
+    public String[][] initializeBoard(String[][] board) {
+        int center = board.length / 2 - 1;
+
+        for(int row = 0; row < board.length; ++row) {
+            for (int column = 0; column < board.length; ++column) {
+                if ((row == center && column == center) || (row == center + 1 && column == center + 1)) {
+                    board[row][column] = "W ";
+                } else if ((row == center && column == center + 1) || (row == center + 1 && column == center)) {
+                    board[row][column] = "B ";  
+                } else {
+                    board[row][column] = "_ ";
+                }
+            }
+        }
+
+        return board;
+    }
+
+    public void displayBoard() {
+        for(String[] row : this.board) {
+            for(String position : row) {
+                System.out.print(position);
+            }
+            System.out.println();
+        }
+    }
+}

f0be281eec620c3e02ab8f6a711a3c8b31988ece
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2016-04-06 18:06:01 -0500

    Implemented allowing a human to make a move

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class HumanPlayer extends Player {
     HumanPlayer() {};
     HumanPlayer(String color) {
        super(color); 
+    }
+
+    public void makeMove(Board board) {
+        Integer[] newMove = new Integer[2];
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your move coordinates, row then column, space separated: ");
+        for(int i = 0; i < 2; ++i) {
+            newMove[i] = input.nextInt();
+        }
+
+        move(newMove, board);
     }
 
 }

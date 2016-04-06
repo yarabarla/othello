ecb1c5666a1d8fd60957a16f3e14cd6c7b4849cd
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2016-04-06 18:50:25 -0500

    Implemented strategy pattern and created a randomly choosing move interface

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,6 +1,30 @@
+import java.util.Random;
 public class ComputerPlayer extends Player {
+    private ChoosingStrategy strategy;
+
     ComputerPlayer() {};
-    ComputerPlayer(String color) {
+    ComputerPlayer(String color, ChoosingStrategy strategy) {
         super(color);
+        this.strategy = strategy;
+    }
+
+    public void makeMove(Board board) {
+        Integer[] newMove = strategy.getMove(board);
+        move(newMove, board);
+    }
+}
+
+interface ChoosingStrategy {
+    public Integer[] getMove(Board board);
+}
+
+class RandomStrategy implements ChoosingStrategy {
+    @Override
+    public Integer[] getMove(Board board) {
+        Random generator = new Random();
+        Integer[][] legalMoves = board.getLegalMoves();
+        int moveIndex = generator.nextInt(legalMoves.length);
+        System.out.println("Random: " + moveIndex);
+        return legalMoves[moveIndex];
     }
 }

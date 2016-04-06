6cb26e6752491847c3d3d973b3f87d2ead2d2cb0
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2016-04-06 17:34:59 -0500

    Implemented movement method

---
+++ Player.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+public abstract class Player {
+    public String color;
+
+    public Player(){};
+    public Player(String color) {
+        this.color = color;
+    }
+
+    public void move(Integer[] coordinates, Board board) {
+        boolean moveValid = false;
+        Integer[][] legalMoves = board.getLegalMoves();
+
+        for (Integer[] move : legalMoves) {
+            if (move[0] == coordinates[0] && move[1] == coordinates[1]) {
+                board.changePosition(coordinates, this.color);
+                moveValid = true;
+            }
+        }
+
+        if (!moveValid) {
+            System.out.println("Invalid move. ");
+        }
+    }
+}
